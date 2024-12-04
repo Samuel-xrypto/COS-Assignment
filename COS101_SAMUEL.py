0f41e157074f4b35f98e4aaaac8699eea1b254bd
Samuel-xrypto <agbeje.agbeje@binghamuni.edu.ng>
2024-12-04 23:49:52 +0100

    Add files via upload

---
+++ COS101_SAMUEL.py
@@ -0,0 +1,10 @@
+number = [21, 22, 23, 24, 25]
+school_list = ['milo',
+               'milk',
+               'sugar',
+               'groundnut',
+               'garri',
+               ]
+print(school_list)
+school_list.reverse()
+print(school_list)
